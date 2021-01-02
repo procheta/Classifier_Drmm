519c37f6b5250139902252bced4b13652fd7f8ab
procheta <procheta@grove.computing.dcu.ie>
2021-01-02 20:05:56 +0000

    script code updated

--- prepare_result_file.sh
+++ prepare_result_file.sh
@@ -1,3 +1,10 @@
+#$1 classifier output , $2 groundTruthFile
+if [ $# -lt 2 ]
+then
+	echo "classifier output file and ground truth file required"
+	exit
+fi
+
 file=$1
 gtFile=$2
 awk '{if ($3 >0.5) {print $1" "$2" 1"}else{print $1" "$2" 0"}}' $file > preprocessed.txt
