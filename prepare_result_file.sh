627ad8d80c34519ef228e43239b08da6f34aa58e
procheta <procheta@grove.computing.dcu.ie>
2021-01-06 12:56:59 +0000

    Code updated

--- prepare_result_file.sh
+++ prepare_result_file.sh
@@ -9,5 +9,5 @@ file=$1
 gtFile=$2
 awk '{if ($3 >0.5) {print $1" "$2" 1"}else{print $1" "$2" 0"}}' $file > preprocessed.txt
 javac DRMMPreprocessing.java
-java DRMMPreprocessing $2 $gtFile result.out 
+java DRMMPreprocessing $gtFile preprocessed.txt  result.out 
 
